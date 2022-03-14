d6c0b689a11c93699162e184355c56148b1ce5e8
runner <runner@Mac-1647251221824.local>
2022-03-14 10:16:35 +0000

    XPoint SDK 3.23.3-release.4118

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "XPointSDK",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "XPointSDK",
+            targets: ["XPointSDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "XPointSDK",
+            url: "https://downloads.xpoint.tech/release/XPointSDKXC-3.23.3-release.4118.zip",
+            checksum: "4fc626a00789a3a1159c593ed58ae464da5dfae334050a806c08661ddfbac421"
+        ),
+    ]
+)
