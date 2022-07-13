e83368d045b77b969b3b7e046ce71c605083b61f
runner <runner@Mac-1657723142212.local>
2022-07-13 15:01:31 +0000

    XPoint SDK 3.24.0

--- Package.swift
+++ Package.swift
@@ -12,11 +12,14 @@ let package = Package(
             targets: ["XPointSDK"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/bugfender/BugfenderSDK-iOS.git", "1.0.0" ..< "2.0.0")
+    ],
     targets: [
         .binaryTarget(
             name: "XPointSDK",
-            url: "https://downloads.xpoint.tech/XPointSDKXC-3.23.100.zip",
-            checksum: "db80597332b9423c715c0ad97ca91b758319792c62887b943af7152289571f92"
+            url: "https://downloads.xpoint.tech/XPointSDKXC-3.24.0.zip",
+            checksum: "c3d25a338bd538ee86b5a8004a8f34bf9528b432c17a97c5a3bd343354487689"
         ),
     ]
 )
