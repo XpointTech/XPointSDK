4e4a14918a453fe805f4747bfaf3c14801744aaa
runner <runner@Mac-1686612719548.local>
2023-06-13 00:24:54 +0000

    XPoint SDK 4.4.0+7150

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "XPointSDK",
-            url: "https://downloads.xpoint.tech/XPointSDKXC-4.1.3.zip",
-            checksum: "e68e05f432c2fa4b79d0b773f2a08e691cca356d65f7d45c627a295b9a5dd1e0"
+            url: "https://downloads.xpoint.tech/XPointSDKXC-4.4.0+7150.zip",
+            checksum: "c3bd46033f2664bf8789e751bbde177650e6b0a0c701c86caa14e1431b77ebf4"
         ),
     ]
 )
