1c3acedd0a68144623bfdb8f8d7d9a73335df38b
Alex Vyrov <alexander.vyrov@xpoint.tech>
2023-10-25 16:26:24 +0400

    Xpoint SDK 4.5.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "XPointSDK",
-            url: "https://downloads.xpoint.tech/XPointSDKXC-4.1.3.zip",
-            checksum: "e68e05f432c2fa4b79d0b773f2a08e691cca356d65f7d45c627a295b9a5dd1e0"
+            url: "https://downloads.xpoint.tech/XPointSDKXC-4.5.0+7905.zip",
+            checksum: "12321acadb5e8273962e42c2f192ed4f1002715b587138cb610b61e05262226d"
         ),
     ]
 )
